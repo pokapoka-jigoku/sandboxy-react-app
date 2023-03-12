8ad1d2a370d895556596333184bc0edc09443afa
pokapoka-jigoku <pokapoka.jigoku@gmail.com>
2023-03-12 17:46:04 +0900

    prepare dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,75 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="title">Vite + React</h1>
-      <div className="box">
-        <div className="container m-2">
-          Count is {count}
+        <h1 className="title">Katan Dashboard</h1>
+        <div className="box">
+          <div className="card">
+            ここに情報がはいる。
+          </div>
+          <div className="buttons m-3">
+            <button onClick={() => setCount((count) => count + 1)} className="button is-primary is-rounded m-2">
+              (+)
+            </button>
+            <button onClick={() => setCount((count) => count + 1)} className="button is-primary is-rounded m-2">
+              (+)
+            </button>
+          </div>
+          <div className="box">
+            <div className="columns is-mobile is-variable is-1">
+              <div className="column mx-2">🐑</div>
+              <div className="column mx-2">羊毛</div>
+              <div className="column mx-2">＊個</div>
+              <button className="column button is-small is-primary mx-2">＋</button>
+              <button className="column button is-small is-primary mx-2">−</button>
+            </div>
+            <div className="columns is-mobile is-variable is-1">
+              <div className="column mx-2">🐑</div>
+              <div className="column mx-2">羊毛</div>
+              <div className="column mx-2">＊個</div>
+              <button className="column button is-small is-primary mx-2">＋</button>
+              <button className="column button is-small is-primary mx-2">−</button>
+            </div>
+            <div className="columns is-mobile is-variable is-1">
+              <div className="column mx-2">🐑</div>
+              <div className="column mx-2">羊毛</div>
+              <div className="column mx-2">＊個</div>
+              <button className="column button is-small is-primary mx-2">＋</button>
+              <button className="column button is-small is-primary mx-2">−</button>
+            </div>
+            <div className="columns is-mobile is-variable is-1">
+              <div className="column mx-2">🐑</div>
+              <div className="column mx-2">羊毛</div>
+              <div className="column mx-2">＊個</div>
+              <button className="column button is-small is-primary mx-2">＋</button>
+              <button className="column button is-small is-primary mx-2">−</button>
+            </div>
+            <div className="columns is-mobile is-variable is-1">
+              <div className="column mx-2">🐑</div>
+              <div className="column mx-2">羊毛</div>
+              <div className="column mx-2">＊個</div>
+              <button className="column button is-small is-primary mx-2">＋</button>
+              <button className="column button is-small is-primary mx-2">−</button>
+            </div>
+          </div>
+          <div className="buttons m-3">
+            <button onClick={() => setCount((count) => count + 1)} className="button is-primary is-rounded m-2">
+              (+)
+            </button>
+            <button onClick={() => setCount((count) => count + 1)} className="button is-primary is-rounded m-2">
+              (+)
+            </button>
+            <button onClick={() => setCount((count) => count + 1)} className="button is-primary m-2">
+              (+)
+            </button>
+            <button onClick={() => setCount((count) => count - 1)} className="button is-danger m-2">
+              (-)
+            </button>
+          </div>
         </div>
-        <div className="container m-2">
-          <button onClick={() => setCount((count) => count + 1)} className="button is-primary m-2">
-            (+)
-          </button>
-          <button onClick={() => setCount((count) => count - 1)} className="button is-danger m-2">
-            (-)
-          </button>
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR. And hello, world.
+        <p className="footer">
+          開発中のアプリのため、動作の保証はありません。
         </p>
       </div>
-      <p className="read-the-docs footer">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
